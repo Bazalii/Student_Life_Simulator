42ff0a326414d8b40cab73b4bb02f5a96a9decfb
Kirill <kirill.zheludev@yandex.ru>
2021-07-26 22:48:48 +0300

    docs: add enum classes

---
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/Song.kt
@@ -0,0 +1,9 @@
+package android.clicker.school_live_simulator
+
+enum class Song(val happiness: Int) {
+    SINGLE_CHORD(100),
+    GAZ_SECTOR(500),
+    SMELLS_LIKE(1000),
+    JAZZ(2500),
+    BAXA(5000)
+}
