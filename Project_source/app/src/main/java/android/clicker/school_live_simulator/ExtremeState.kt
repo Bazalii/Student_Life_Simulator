8f32b76fdeccc28191cb46f5e8d2bdfac8abe46d
Andrey Novikov <andrewnovicov@gmail.com>
2021-07-30 18:14:09 +0300

    feat: add PlayerState inheritors Note: properties have default values

---
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/ExtremeState.kt
@@ -0,0 +1,7 @@
+package android.clicker.school_live_simulator
+
+class ExtremeState : PlayerState() {
+    override val reduce_school_performance_percent: Int = 0
+    override val reduce_happines_percent: Int = 0
+    override val reduce_satiety_percent: Int = 0
+}
