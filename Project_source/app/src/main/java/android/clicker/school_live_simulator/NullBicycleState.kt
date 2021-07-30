2c615a72e558da545e7a6578a3034487fd6ed148
Andrey Novikov <andrewnovicov@gmail.com>
2021-07-30 14:36:35 +0300

    feat: add subclass NullBicycleState Implementations: - override fun changeState - list available_delivery remains empty
    Reasons for that:
    - Player cannot deliver in this state, so he has empty available_delivery list

---
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/NullBicycleState.kt
@@ -0,0 +1,8 @@
+package android.clicker.school_live_simulator
+
+open class NullBicycleState : BicycleState() {
+    override val price: Int = 0
+    override fun changeState(bag: Player.Bag) {
+        bag.bicycle = USSR_bicycleState()
+    }
+}
