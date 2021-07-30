ec746a50e0f9a79edaebe478beb832648740de64
Andrey Novikov <andrewnovicov@gmail.com>
2021-07-30 15:40:31 +0300

    fix(NullBicycleState.kt): rename class - USSR_BicycleState -> UssrBicycleState

--- Project_source/app/src/main/java/android/clicker/school_live_simulator/NullBicycleState.kt
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/NullBicycleState.kt
@@ -3,6 +3,6 @@ package android.clicker.school_live_simulator
 open class NullBicycleState : BicycleState() {
     override val price: Int = 0
     override fun changeState(bag: Player.Bag) {
-        bag.bicycle = USSR_bicycleState()
+        bag.bicycle = UssrBicycleState()
     }
 }
