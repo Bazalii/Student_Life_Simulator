74dad588c31998f56fd7fa2a0edc860dd156106a
Andrey Novikov <andrewnovicov@gmail.com>
2021-07-31 23:26:32 +0300

    docs: add commentary Explain what the class does and when its values are used

--- Project_source/app/src/main/java/android/clicker/school_live_simulator/PlayerState.kt
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/PlayerState.kt
@@ -3,6 +3,10 @@ package android.clicker.school_live_simulator
  * @author      Andrey Novikov andrewnovicov@gmail.com
  * @version     1.0                 (current version number of program)
  * @since       1.0          (the version of the package this class was first added to)
+ * Every tick player characteristics: school_performance, happiness and satiety reduces.
+ * 3 subclasses of abstract class define different player state.
+ * Each player state defines its constants.
+ * These constants define player characteristics reduce.
  */
 abstract class PlayerState {
     protected abstract val reduce_school_performance_percent: Int
