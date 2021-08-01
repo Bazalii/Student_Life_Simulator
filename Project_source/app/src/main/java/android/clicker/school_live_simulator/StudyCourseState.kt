e2624a5aa8d2c1c0b3b40d826e0890aef7803ea8
Kirill <kirill.zheludev@yandex.ru>
2021-08-01 15:37:42 +0300

    style: delete comments about author and version

--- Project_source/app/src/main/java/android/clicker/school_live_simulator/StudyCourseState.kt
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/StudyCourseState.kt
@@ -1,11 +1,5 @@
 package android.clicker.school_live_simulator
 
-/**
- * @author      Kirill Zheludev kirill.zheludev@yandex.ru
- * @version     1.0                 (current version number of program)
- * @since       1.0          (the version of the package this class was first added to)
- */
-
 abstract class StudyCourseState: TimerObserver {
     /**
 	 * Price of course
