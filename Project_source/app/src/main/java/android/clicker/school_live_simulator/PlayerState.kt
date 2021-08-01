f4dd380a2e19c1855c56348cffaac8b57252717f
Andrey Novikov <andrewnovicov@gmail.com>
2021-08-01 14:38:18 +0300

    fix: change commentary - remove filed with author, since and version

--- Project_source/app/src/main/java/android/clicker/school_live_simulator/PlayerState.kt
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/PlayerState.kt
@@ -1,8 +1,5 @@
 package android.clicker.school_live_simulator
 /**
- * @author      Andrey Novikov andrewnovicov@gmail.com
- * @version     1.0                 (current version number of program)
- * @since       1.0          (the version of the package this class was first added to)
  * Every tick player characteristics: school_performance, happiness and satiety reduces.
  * 3 subclasses of abstract class define different player state.
  * Each player state defines its constants.
