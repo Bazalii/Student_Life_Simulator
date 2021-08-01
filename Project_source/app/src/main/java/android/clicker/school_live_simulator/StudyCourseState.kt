45889a02414de1d8db7e165f37c5cf423bfbe2db
Kirill <kirill.zheludev@yandex.ru>
2021-08-01 13:46:08 +0300

    style and fix: add commentaries and newlines; change access type for field of StudyCourseState

--- Project_source/app/src/main/java/android/clicker/school_live_simulator/StudyCourseState.kt
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/StudyCourseState.kt
@@ -1,13 +1,27 @@
 package android.clicker.school_live_simulator
 
+/**
+ * @author      Kirill Zheludev kirill.zheludev@yandex.ru
+ * @version     1.0                 (current version number of program)
+ * @since       1.0          (the version of the package this class was first added to)
+ */
+
 abstract class StudyCourseState: TimerObserver {
-    abstract val price: Int
+    /**
+	 * Price of course
+	*/
+	protected abstract val price: Int
+	
+	/**
+	 * For student change current course
+	 * @param student who change course
+	*/
     abstract fun buyNextCourse(var student: Player) {
-
     }
+	
     abstract override fun timerTickHandler() {
-
     }
+	
     abstract override fun timerEndHandler() {
     }
 	
