aa9466739c78422ecf338385b72f669706bbcf84
Kirill <kirill.zheludev@yandex.ru>
2021-08-01 16:33:47 +0300

    style: add documentation comments

--- Project_source/app/src/main/java/android/clicker/school_live_simulator/TimerObserver.kt
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/TimerObserver.kt
@@ -1,8 +1,15 @@
 package android.clicker.school_live_simulator
 
 interface TimerObserver {
+	/**
+	 * Function which response 
+	 * for timer tick 
+	*/
     fun timerTickHandler()
 	
+	/**
+	 * Function which end timer
+	*/
     fun timerEndHandler()
 	
 }
