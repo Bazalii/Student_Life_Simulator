02f1575f5615ad98aa55be68b3bcacfadbf04d9e
Kirill <kirill.zheludev@yandex.ru>
2021-08-02 11:54:22 +0300

    style: fix grammatical error

--- Project_source/app/src/main/java/android/clicker/school_live_simulator/TimerObserver.kt
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/TimerObserver.kt
@@ -2,7 +2,7 @@ package android.clicker.school_live_simulator
 
 interface TimerObserver {
 	/**
-	 * Handler who will be risen  
+	 * Handler which will be risen  
 	 * when timer will change time on one point
 	 */
     fun timerTickHandler()
