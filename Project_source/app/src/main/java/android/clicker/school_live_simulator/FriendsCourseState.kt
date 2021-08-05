1d3fb4eeef4f54f4b5cc312fb702275a594a6c7a
yevgeniy <yevgeniysudakov@gmail.com>
2021-08-05 18:33:40 +0300

    feat: add class FriendsCourseState

---
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/FriendsCourseState.kt
@@ -0,0 +1,20 @@
+package android.clicker.school_live_simulator
+
+open class FriendsCourseState : NullComputerCourseState() {
+    override val price: Int = 0
+
+    init {
+        available_web_task.add(WebTask.ACCOUNT_BOOST)
+    }
+
+    override fun buyNexCourse(courses: Player.Courses) {
+        courses.computer_course = OnlineWorkCourseState()
+    }
+
+    override fun timerTickHandler(){
+
+    }
+    override fun timerEndHandler(){
+
+    }
+}
