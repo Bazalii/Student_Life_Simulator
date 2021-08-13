f12ae860a2cd4870887655b0719b6f4b0ae7c1d5
Andrey Novikov <andrewnovicov@gmail.com>
2021-08-13 15:55:43 +0300

    feat: Shop fragment UI enhancement - good are categorised - click on category button will drop a list of available goods

--- Project_source/app/src/main/java/android/clicker/school_live_simulator/User_interface/ScrollingFragments/ShopScrollingFragment.kt
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/User_interface/ScrollingFragments/ShopScrollingFragment.kt
@@ -5,6 +5,8 @@ import android.clicker.school_live_simulator.databinding.FragmentShopScrollingBi
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
@@ -20,6 +22,87 @@ class ShopScrollingFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        binding.shopGuitarCourses.setOnClickListener {
+            if(binding.shopGuitarCourseV1.visibility == GONE){
+                binding.shopGuitarCourseV1.visibility = VISIBLE
+                binding.shopGuitarCourseV2.visibility = VISIBLE
+                binding.shopGuitarCourseV3.visibility = VISIBLE
+                binding.shopGuitarCourseV4.visibility = VISIBLE
+                binding.shopGuitarCourseV5.visibility = VISIBLE
+            }
+            else{
+                binding.shopGuitarCourseV1.visibility = GONE
+                binding.shopGuitarCourseV2.visibility = GONE
+                binding.shopGuitarCourseV3.visibility = GONE
+                binding.shopGuitarCourseV4.visibility = GONE
+                binding.shopGuitarCourseV5.visibility = GONE
+            }
+        }
+
+        binding.shopComputerCourses.setOnClickListener {
+            if(binding.shopComputerCourseV1.visibility == GONE){
+                binding.shopComputerCourseV1.visibility = VISIBLE
+                binding.shopComputerCourseV2.visibility = VISIBLE
+                binding.shopComputerCourseV3.visibility = VISIBLE
+                binding.shopComputerCourseV4.visibility = VISIBLE
+                binding.shopComputerCourseV5.visibility = VISIBLE
+            }
+            else{
+                binding.shopComputerCourseV1.visibility = GONE
+                binding.shopComputerCourseV2.visibility = GONE
+                binding.shopComputerCourseV3.visibility = GONE
+                binding.shopComputerCourseV4.visibility = GONE
+                binding.shopComputerCourseV5.visibility = GONE
+            }
+        }
+
+        binding.shopBicycles.setOnClickListener {
+            if(binding.shopUssrBicycle.visibility == GONE){
+                binding.shopUssrBicycle.visibility = VISIBLE
+                binding.shopUsualBicycle.visibility = VISIBLE
+                binding.shopMountainBicycle.visibility = VISIBLE
+            }
+            else{
+                binding.shopUssrBicycle.visibility = GONE
+                binding.shopUsualBicycle.visibility = GONE
+                binding.shopMountainBicycle.visibility = GONE
+            }
+        }
+
+        binding.shopGuitars.setOnClickListener {
+            if(binding.shopUssrGuitar.visibility == GONE){
+                binding.shopUssrGuitar.visibility = VISIBLE
+                binding.shopUralGuitar.visibility = VISIBLE
+                binding.shopYamahaGuitar.visibility = VISIBLE
+                binding.shopFenderGuitar.visibility = VISIBLE
+                binding.shopDoubleNeckGuitar.visibility = VISIBLE
+            }
+            else{
+                binding.shopUssrGuitar.visibility = GONE
+                binding.shopUralGuitar.visibility = GONE
+                binding.shopYamahaGuitar.visibility = GONE
+                binding.shopFenderGuitar.visibility = GONE
+                binding.shopDoubleNeckGuitar.visibility = GONE
+            }
+        }
+
+        binding.shopComputers.setOnClickListener {
+            if(binding.shopPreviouslyUsedComputer.visibility == GONE){
+                binding.shopPreviouslyUsedComputer.visibility = VISIBLE
+                binding.shopOldComputer.visibility = VISIBLE
+                binding.shopUsualComputer.visibility = VISIBLE
+                binding.shopXiaomiComputer.visibility = VISIBLE
+                binding.shopMacbookComputer.visibility = VISIBLE
+            }
+            else{
+                binding.shopPreviouslyUsedComputer.visibility = GONE
+                binding.shopOldComputer.visibility = GONE
+                binding.shopUsualComputer.visibility = GONE
+                binding.shopXiaomiComputer.visibility = GONE
+                binding.shopMacbookComputer.visibility = GONE
+            }
+        }
 
     }
 }
