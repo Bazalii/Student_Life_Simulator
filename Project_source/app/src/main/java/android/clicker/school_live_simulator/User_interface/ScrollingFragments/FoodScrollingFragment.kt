ec3527d6125590d18a235cf661e1943bf5bf30b0
Andrey Novikov <andrewnovicov@gmail.com>
2021-08-13 15:49:08 +0300

    feat: add some strings and implement food buttons logic - all buttons in Food have correct logic - add try-catch to every button in Food

--- Project_source/app/src/main/java/android/clicker/school_live_simulator/User_interface/ScrollingFragments/FoodScrollingFragment.kt
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/User_interface/ScrollingFragments/FoodScrollingFragment.kt
@@ -1,14 +1,18 @@
 package android.clicker.school_live_simulator.User_interface.ScrollingFragments
 
+import android.clicker.school_live_simulator.Classes.IncorrectValueException
 import android.clicker.school_live_simulator.Food
 import android.clicker.school_live_simulator.Game
+import android.clicker.school_live_simulator.R
 import android.clicker.school_live_simulator.User_interface.GameActivity
 import android.clicker.school_live_simulator.databinding.FragmentFoodScrollingBinding
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
+import java.lang.Exception
 
 class FoodScrollingFragment : Fragment() {
     lateinit var binding: FragmentFoodScrollingBinding
@@ -23,33 +27,58 @@ class FoodScrollingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         binding.foodEatCrackers.setOnClickListener{
-            Game.player.eat(Food.CRACKERS)
-            (activity as GameActivity).updateStats()
+            try {
+                Game.player.eat(Food.CRACKERS)
+                (activity as GameActivity).updateStats()
+            } catch (exception: IncorrectValueException){
+                binding.foodEatCrackers.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake))
+            }
         }
 
         binding.foodEatDoshirack.setOnClickListener{
-            Game.player.eat(Food.DOSHIRAK)
-            (activity as GameActivity).updateStats()
+            try {
+                Game.player.eat(Food.DOSHIRAK)
+                (activity as GameActivity).updateStats()
+            } catch (exception: IncorrectValueException){
+                binding.foodEatDoshirack.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake))
+            }
+
         }
 
         binding.foodEatAtTheCanteen.setOnClickListener{
-            Game.player.eat(Food.EATERY_FOOD)
-            (activity as GameActivity).updateStats()
+            try {
+                Game.player.eat(Food.EATERY_FOOD)
+                (activity as GameActivity).updateStats()
+            } catch (exception: IncorrectValueException){
+                binding.foodEatAtTheCanteen.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake))
+            }
         }
 
         binding.foodEatAtMcdonalds.setOnClickListener{
-            Game.player.eat(Food.MCDONALDS)
-            (activity as GameActivity).updateStats()
+            try {
+                Game.player.eat(Food.MCDONALDS)
+                (activity as GameActivity).updateStats()
+            } catch (exception: IncorrectValueException){
+                binding.foodEatAtMcdonalds.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake))
+            }
         }
 
         binding.foodOrderDelivery.setOnClickListener{
-            Game.player.eat(Food.PIZZA_AT_HOME)
-            (activity as GameActivity).updateStats()
+            try {
+                Game.player.eat(Food.PIZZA_AT_HOME)
+                (activity as GameActivity).updateStats()
+            } catch (exception: IncorrectValueException){
+                binding.foodOrderDelivery.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake))
+            }
         }
 
         binding.foodGoToTheRestaurant.setOnClickListener{
-            Game.player.eat(Food.RESTORANT_FOOD)
-            (activity as GameActivity).updateStats()
+            try {
+                Game.player.eat(Food.RESTORANT_FOOD)
+                (activity as GameActivity).updateStats()
+            } catch (exception: IncorrectValueException){
+                binding.foodGoToTheRestaurant.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake))
+            }
         }
 
     }
