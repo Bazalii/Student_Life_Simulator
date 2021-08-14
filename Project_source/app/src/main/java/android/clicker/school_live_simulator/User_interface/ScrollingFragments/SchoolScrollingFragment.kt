cf7c1d019ccc51948731933b75ab0633f905437f
Andrey Novikov <andrewnovicov@gmail.com>
2021-08-14 19:42:13 +0300

    feat: add logic for clicks to SchoolScrollingFragment.kt

--- Project_source/app/src/main/java/android/clicker/school_live_simulator/User_interface/ScrollingFragments/SchoolScrollingFragment.kt
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/User_interface/ScrollingFragments/SchoolScrollingFragment.kt
@@ -1,13 +1,17 @@
 package android.clicker.school_live_simulator.User_interface.ScrollingFragments
 
+import android.clicker.school_live_simulator.Classes.Enum_classes.Studies
+import android.clicker.school_live_simulator.Classes.IncorrectValueException
 import android.clicker.school_live_simulator.Food
 import android.clicker.school_live_simulator.Game
+import android.clicker.school_live_simulator.R
 import android.clicker.school_live_simulator.User_interface.GameActivity
 import android.clicker.school_live_simulator.databinding.FragmentSchoolScrollingBinding
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 
 class SchoolScrollingFragment : Fragment() {
@@ -23,33 +27,66 @@ class SchoolScrollingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.schoolGoToSchool.setOnClickListener{
-            //Some logic to write
-            (activity as GameActivity).updateStats()
+            try {
+                Game.player.study(Studies.GO_TO_SCHOOL)
+                (activity as GameActivity).updateStats()
+            }catch (exception: IncorrectValueException){
+                binding.schoolGoToSchool.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake))
+            }
+
         }
 
         binding.schoolBotat.setOnClickListener{
-            //Some logic to write
-            (activity as GameActivity).updateStats()
+            try {
+                Game.player.study(Studies.LEARN_YOURSELF)
+                (activity as GameActivity).updateStats()
+            }catch (exception: IncorrectValueException){
+                binding.schoolBotat.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake))
+            }
         }
 
         binding.schoolBuyCheatbook.setOnClickListener{
-            //Some logic to write
-            (activity as GameActivity).updateStats()
+            try {
+                Game.player.study(Studies.BUY_CHEATBOOK)
+                (activity as GameActivity).updateStats()
+            }catch (exception: IncorrectValueException){
+                binding.schoolBuyCheatbook.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake))
+            }
         }
 
-        binding.schoolHireATutor.setOnClickListener{
-            //Some logic to write
-            (activity as GameActivity).updateStats()
+        binding.schoolLearnFromStudent.setOnClickListener{
+            try {
+                Game.player.study(Studies.LEARN_FROM_A_STUDENT)
+                (activity as GameActivity).updateStats()
+            }catch (exception: IncorrectValueException){
+                binding.schoolLearnFromStudent.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake))
+            }
+        }
+        binding.schoolHireAGoodTutor.setOnClickListener{
+            try {
+                Game.player.study(Studies.HIRE_A_GOOD_TUTOR)
+                (activity as GameActivity).updateStats()
+            }catch (exception: IncorrectValueException){
+                binding.schoolHireAGoodTutor.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake))
+            }
         }
 
         binding.schoolSignUpInAnOnlineSchool.setOnClickListener{
-            //Some logic to write
-            (activity as GameActivity).updateStats()
+            try {
+                Game.player.study(Studies.SIGN_UP_IN_AN_ONLINE_SCHOOL)
+                (activity as GameActivity).updateStats()
+            }catch (exception: IncorrectValueException){
+                binding.schoolSignUpInAnOnlineSchool.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake))
+            }
         }
 
         binding.schoolStudyWithUniTeacher.setOnClickListener{
-            //Some logic to write
-            (activity as GameActivity).updateStats()
+            try {
+                Game.player.study(Studies.STUDY_WITH_UNI_TEACHER)
+                (activity as GameActivity).updateStats()
+            }catch (exception: IncorrectValueException){
+                binding.schoolStudyWithUniTeacher.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.shake))
+            }
         }
 
     }
