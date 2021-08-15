17e1ec34caeffd32e86da8975819b382813efe91
Andrey Novikov <andrewnovicov@gmail.com>
2021-08-15 22:14:17 +0300

    feat: buttons in Work Fragment change their text according to available work

--- Project_source/app/src/main/java/android/clicker/school_live_simulator/User_interface/ScrollingFragments/WorkScrollingFragment.kt
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/User_interface/ScrollingFragments/WorkScrollingFragment.kt
@@ -1,5 +1,6 @@
 package android.clicker.school_live_simulator.User_interface.ScrollingFragments
 
+import android.clicker.school_live_simulator.*
 import android.clicker.school_live_simulator.User_interface.GameActivity
 import android.clicker.school_live_simulator.databinding.FragmentWorkScrollingBinding
 import android.os.Bundle
@@ -60,5 +61,66 @@ class WorkScrollingFragment : Fragment() {
             //Some logic to write
             (activity as GameActivity).updateStats()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        when(Game.player.items.bicycle){
+            is MountainBicycleState ->{
+                binding.workAsACourier.text =
+            }
+            is UsualBicycleState->{
+                binding.workAsACourier.text =
+            }
+            is UssrBicycleState->{
+                binding.workAsACourier.text =
+            }
+            else->{
+                binding.workAsACourier.text =
+            }
+        }
+
+        when(Game.player.current_courses.guitar_course){
+            is MusicalObservatoryCourseState ->{
+               binding.workPlayGuitarV1.text =
+            }
+            is MusicalSchoolCourseState ->{
+                binding.workPlayGuitarV1.text =
+            }
+            is YardSongCourseState ->{
+                binding.workPlayGuitarV1.text =
+            }
+            is FirstSongCourseState ->{
+                binding.workPlayGuitarV1.text =
+            }
+            is YardGuitarCourseState ->{
+                binding.workPlayGuitarV1.text =
+            }
+            else ->{
+                binding.workPlayGuitarV1.text =
+            }
+        }
+        when(Game.player.current_courses.computer_course){
+            is GameDevelopmentCourseState ->{
+                binding.workOnTheNetV1.text =
+            }
+            is VideoEditingCourseState ->{
+                binding.workOnTheNetV1.text =
+            }
+            is WebDesignCourseState ->{
+                binding.workOnTheNetV1.text =
+            }
+            is OnlineWorkCourseState ->{
+                binding.workOnTheNetV1.text =
+            }
+            is FriendsCourseState ->{
+                binding.workOnTheNetV1.text =
+            }
+            else ->{
+                binding.workOnTheNetV1.text = //maybe text like pass the course previously
+            }
+        }
+
+
     }
 }
