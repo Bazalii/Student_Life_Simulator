827e2dbc3ab2ecf9e1afd00ef0d970dfbb752d0e
Bazalii Ivan <bazalijivan2002@yandex.ru>
2021-08-20 15:36:05 +0300

    feat: add getNumber method in ContextBundle
    - Change get title, it returns String
    - Add new method for gettig Number from strings.xml

--- Project_source/app/src/main/java/android/clicker/school_live_simulator/Classes/Main/ContextBundle.kt
+++ Project_source/app/src/main/java/android/clicker/school_live_simulator/Classes/Main/ContextBundle.kt
@@ -7,7 +7,11 @@ import android.content.Context
 class ContextBundle {
     lateinit var context: Context
 
-    fun getTitle(input: String): Int {
+    fun getTitle(input: String): String {
+        return Game.context_bundle.context.resources.getString(Game.context_bundle.context.resources.getIdentifier(input, "string", context.getPackageName()))
+    }
+
+    fun getNumber(input: String): Int {
         return Game.context_bundle.context.resources.getString(Game.context_bundle.context.resources.getIdentifier(input, "string", context.getPackageName())).toInt()
     }
 }
